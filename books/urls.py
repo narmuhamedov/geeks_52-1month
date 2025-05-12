76f8f253f674011b2faedb113c0efbd01c48ddcd
sofware-engeneer <kingradomir@gmail.com>
2025-05-12 18:27:40 +0600

    lesson4

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
   path ('book_list/', views.books_list_view, name='book_list'),
   path ('book_list/<int:id>/', views.book_detail_view, name='book_detail'),
+  path ('book_list/<int:id>/delete/', views.delete_book_view, name='delete_book'),
+  path ('book_list/<int:id>/update/', views.update_book_view, name='update_book'),
+  path ('create_book/', views.create_book_view, name='create_book'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,54 @@
-from django.shortcuts import render, get_object_or_404
-from . import models
+from django.shortcuts import render, get_object_or_404, redirect
+from . import models, forms
+from django.http import HttpResponse
 
+
+#CRUD - Create, Read, Update, Delete
+
+
+#update book
+def update_book_view(request,id):
+    book_id = get_object_or_404(models.Book, id=id)
+    if request.method == "POST":
+        form  = forms.BookForm(request.POST, instance=book_id)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+            #return HttpResponse('Книга успешно обновлена')
+    else:
+        form = forms.BookForm(instance=book_id)
+    return render(request, template_name='books/update_book.html', context={
+        'form': form,
+        'book_id': book_id,
+        })
+
+
+# Delete book
+def delete_book_view(request, id):
+    book_id = get_object_or_404(models.Book, id=id)
+    book_id.delete()
+    return redirect('book_list')
+    #return HttpResponse('Книга успешно удалена')
+
+
+
+#create_book
+def create_book_view(request):
+    if request.method == 'POST':
+        form = forms.BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+            #return HttpResponse('Книга успешно добавлена')
+    else:
+        form = forms.BookForm()
+    return render(request, template_name='books/create_book.html', context={'form': form})
+
+
+
+
+
+#read 
 def books_list_view(request):
     if request.method == 'GET':
         #Запрос из базы данных переменная book запрашивает данные у БД
